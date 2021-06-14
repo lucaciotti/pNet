fe650842cb2ce40af1cddc603d0c5c5a2236682e
Luca <luca.ciotti@gmail.com>
2021-06-14 09:18:39 +0200

    Ops migration failed

--- database/migrations/2021_06_14_071724_add_inviatoemail_users.php
+++ database/migrations/2021_06_14_071724_add_inviatoemail_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnUsers extends Migration
+class AddInviatoemailUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddColumnUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('invitato_email');
+            $table->boolean('invitato_email')->default(false);
         });
     }
 
@@ -26,7 +26,7 @@ class AddColumnUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('invitato_email')->default(false);
+            $table->dropColumn('invitato_email');
         });
     }
 }
