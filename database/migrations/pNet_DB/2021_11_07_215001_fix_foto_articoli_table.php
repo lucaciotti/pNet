4f33398747b84d25d7f57daa6a7bb69c6cf84990
Luca <luca.ciotti@gmail.com>
2021-11-07 22:58:19 +0100

    ERROR: Fix table Articoli

---
+++ database/migrations/pNet_DB/2021_11_07_215001_fix_foto_articoli_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixFotoArticoliTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articoli', function (Blueprint $table) {
+            $table->string('nome_foto2', 250)->change();
+            $table->string('nome_foto3', 250)->change();
+            $table->string('nome_foto4', 250)->change();
+            $table->string('nome_foto5', 250)->change();
+            $table->string('file_tecn', 250)->change();
+            $table->string('file_sic', 250)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articoli', function (Blueprint $table) {
+            //
+        });
+    }
+}
