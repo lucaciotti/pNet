6e38946fc615a4daaf966afc59ddb53271ea6b2b
Luca Ciotti <luca.ciotti@gmail.com>
2023-05-10 19:51:12 +0200

    ImportCSV Modal

--- resources/views/livewire/cart/import-csv.blade.php
+++ resources/views/livewire/cart/import-csv.blade.php
@@ -10,5 +10,12 @@
     <div>
         <button type="submit" @if($importfromDoc) disabled @endif class="btn bg-lightblue btn-sm btn-block" style="margin-top:10px;">Importa</button>
     </div>
-
+    <hr>
+    <div class="text-sm text-center">
+        <h6>Crea un ordine a partire da un file CSV</h6>
+        <p>Il file CSV deve contenere una prima colonna con il codice del prodotto di Ferramenta Paride e una seconda colonna
+            con la quantità del prodotto.</p>
+        <a href="{{ public_path()." /download/cart_example.csv" }}" download class="text-primary"><i
+                class="fa fa-download"></i>Scarica file di esempio</a>
+    </div>
 </form>
