0b1c63e0984c00ee39813270a5cb1dba789f5705
Luca Ciotti <luca.ciotti@gmail.com>
2023-06-16 15:15:03 +0200

    Fix Mail Cli Preordine

--- resources/views/parideViews/_emails/docs/xwToSendToCli.blade.php
+++ resources/views/parideViews/_emails/docs/xwToSendToCli.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Buongiorno, {{ $client->rag_soc }} [{{ $client->id_cli_for }}@pnet.it] 
-con questa email automatica le inviamo in allegato il documento Ordine-Web emesso da pNet, il portale extranet di [Ferramenta Paride](https://www.ferramentaparide.it/). 
-Il dettagli del presente documento (articoli, prezzi, disponibilità, tempi e modalità di consegna) verranno controllati dai nostri operatori e sono da intendersi non vincolanti. 
+con questa email automatica le inviamo in allegato il documento Ordine-Web emesso da pNet, il portale extranet di [Ferramenta Paride](https://www.ferramentaparide.it/).  
+Il dettagli del presente documento (articoli, prezzi, disponibilità, tempi e modalità di consegna) verranno controllati dai nostri operatori e sono da intendersi non vincolanti.  
 Al termine del processo di verifica riceverà conferma d'ordine aggiornata.
 
 Le ricordiamo che tutti i Pre-Ordini della sua azienda sono consultabili attraverso il link qui sotto:
